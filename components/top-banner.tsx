cf771641c891e1a7439c61aa75e7947b32374d75
ojoedejen <dejenojoe@gmail.com>
2026-04-07 16:58:02 +0000

    Update top-banner.tsx

--- components/top-banner.tsx
+++ components/top-banner.tsx
@@ -9,7 +9,7 @@ export default function TopBanner() {
         <div className="flex flex-col sm:flex-row items-center justify-center gap-1 sm:gap-2 text-xs sm:text-sm font-bold font-serif uppercase tracking-wider">
           <div className="flex items-center gap-1 sm:gap-2">
             <Phone className="w-3 h-3 sm:w-4 sm:h-4" />
-            <span>For Feedback please Only Call : +23230431136 | WhatsApp Only: +1 (240) 605-2966 </span>
+            <span>For Feedback please Only Call: +232 7634 0991 | WhatsApp Only: +1 (240) 605-2966 </span>
           </div>
         </div>
       </div>
